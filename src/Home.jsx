9414c307f720fb5d3c2fb221cf9225c23a1a2c41
srshakil2 <rh3642921@gmail.com>
2025-02-06 12:40:08 +0600

    set up done

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold underline bg-blue-700">this is Home</h3>
+      <button className=" btn btn-info">suncdd</button>
+    </div>
+  );
+};
+
+export default Home;
